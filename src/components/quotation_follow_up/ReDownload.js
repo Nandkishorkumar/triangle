b6e67d86be091ee73b3c7da4234e233de08217f3
nand kishor kumar <991221nandkishor@gmail.com>
2022-05-06 17:03:01 +0530

    reset

--- src/components/quotation_follow_up/ReDownload.js
+++ src/components/quotation_follow_up/ReDownload.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Profile/profile.css';
 import jsPDF from 'jspdf';
 const Redownload = (props) => {
+    const path='/assets/img/imgplane.jpg'
     function pdfgenrator() {
         // update_quotation_flg()
         var doc = new jsPDF("p", "pt", "a4");
@@ -135,7 +136,7 @@ const Redownload = (props) => {
                     </div>
                     <div className='details1'>
                         <p style={{ fontWeight: '600' }}>Flight</p>
-                        <img alt='plane' src='/assets/img/imgplane.jpg' width='45px' height='35px' style={{ margin: "1rem", marginTop: "-0.4rem", marginBottom: "-0.7rem" }} />
+                        <img alt='plane' src={path} width='45px' height='35px' style={{ margin: "1rem", marginTop: "-0.4rem", marginBottom: "-0.7rem" }} />
                         <p>{props.cabDetailsData}</p>
                         <p style={{ fontWeight: '600' }}>Cabs</p>
                         <img alt='plane' src='/assets/img/taxi.png' width='50px' height='50px' style={{ margin: "1rem", marginTop: "-0.7rem", marginBottom: "-1rem" }} />
