626401f1e3a14bdb43a489e7c1c3c9ba4d3b1ad7
nand kishor kumar <991221nandkishor@gmail.com>
2022-05-10 12:35:59 +0530

    productiom

---
+++ src/components/tester/Test.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import ReactToPdf from 'react-to-pdf'
+const Test = () => {
+    const ref = React.createRef();
+    
+    return (
+        <div>
+            <ReactToPdf targetRef={ref} filename="div-blue.pdf" >
+                {({ toPdf }) => (
+                    <button onClick={toPdf}>Generate pdf</button>
+                )}
+            </ReactToPdf>
+            <div  ref={ref} >
+                <h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1>
+                <h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1>
+                <h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1>
+                <h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1>
+                <h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1>
+                <h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1><h1>dfhjdhgdfgjkhgkkgdfjkgdfkhgkdgjdfhgjkdhgjkhdghdkghkdhggjkdhvnxcmvbxcnjhgjbdgndfghdfjglg
+                StrictMode is a tool for highlighting potential problems
+                 in an application. Like Fragment, StrictMode does not render any visible UI. 
+                 It activates additional checks and warnings for its descendants.
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Test;
